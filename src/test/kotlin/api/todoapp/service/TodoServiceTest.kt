26a6ed6b44cf6401e377d972a3f7f8ebcaa2da3d
kimbackdoo <kdk7121743@naver.com>
2022-12-20 01:11:03 +0900

    feat: TodoService Test 코드 작성중

---
+++ src/test/kotlin/api/todoapp/service/TodoServiceTest.kt
@@ -0,0 +1,52 @@
+package api.todoapp.service
+
+import api.todoapp.domain.Todo
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@Transactional
+// 매 테스트마다 row 생성 -> 삭제 -> 테스트 순으로 이뤄지는 방법 찾기...
+class TodoServiceTest (@Autowired private val todoService: TodoService) {
+    @Test
+    fun `Todo가 생성된다`() {
+        // given
+        val testTodo = Todo(content="test1", position=1)
+
+        // when
+        val todo = todoService.createTodo(testTodo)
+
+        // then
+        assertThat(todo.id).isEqualTo(1)
+    }
+
+    @Test
+    fun `1개의 Todo를 가져온다`() {
+        // given
+        val testTodo = Todo(content="test1", position=1)
+        todoService.createTodo(testTodo)
+
+        // when
+        val todo = todoService.getTodo(todoId=1)
+
+        // then
+        assertThat(todo).isNotNull
+    }
+
+    @Test
+    fun `Todo가 존재하지 않는다`() {
+        // given
+        val testTodo = Todo(content="test1", position=1)
+        todoService.createTodo(testTodo)
+
+        // when
+        val todo = todoService.getTodo(todoId=2)
+
+        // then
+        assertThat(todo).isNull()
+    }
+
+}
